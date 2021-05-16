7f5db1ecc062300e331caf2f2dcc04e33bc7d3b7
nasim-ahmed <nasim.ahmed54@gmail.com>
2021-05-16 16:47:11 +0200

    changes

--- bert/test_model.py
+++ bert/test_model.py
@@ -21,7 +21,7 @@ def get_explanations(avg):
     if avg > threshold:
         print("The two paragraphs are similar contextually.")
     else:
-        print("The two paragraphs are dissimilar")
+        print("The two paragraphs are dissimilar contextually")
 
 
 
@@ -51,12 +51,9 @@ def get_similarity(para1, para2):
     return avg
 
 if __name__ == '__main__':
-<<<<<<< Updated upstream
-    load_model()
-=======
-
     paragraph1 = 'Multi-objective optimization aims at finding trade-off solutions to conflicting objectives. These constitute the Pareto optimal set. In the context of expensive-to-evaluate functions, it is impossible and often non-informative to look for the entire set. As an end-user would typically prefer a certain part of the objective space, we modify the Bayesian multi-objective optimization algorithm which uses Gaussian Processes to maximize the Expected Hypervolume Improvement, to focus the search in the preferred region. The acumulated effects of the Gaussian Processes and the targeting strategy lead to a particularly efficient convergence to the desired part of the Pareto set. To take advantage of parallel computing, a multi-point extension of the targeting criterion is proposed and analyzed.'
     paragraph2 = 'We consider the problem of constrained multi-objective (MO) blackbox optimization using expensive function evaluations, where the goal is to approximate the true Pareto set of solutions satisfying a set of constraints while minimizing the number of function evaluations. We propose a novel framework named Uncertainty-aware Search framework for Multi-Objective Optimization with Constraints (USeMOC) to efficiently select the sequence of inputs for evaluation to solve this problem. The selection method of UseMOC consists of solving a cheap constrained MO optimization problem via surrogate models of the true functions to identify the most promising candidates and picking the best candidate based on a measure of uncertainty. We applied this framework to optimize the design of a multi-output switched-capacitor voltage regulator via expensive simulations. Our experimental results show that UseMOC is able to achieve more than 90 % reduction in the number of simulations needed to uncover optimized circuits.'
     score = get_similarity(paragraph1, paragraph2)
     get_explanations(score)
->>>>>>> Stashed changes
+
+
